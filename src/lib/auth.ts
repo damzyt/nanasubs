7d8522f2b72e7afa6a176f970ee64648baf51761
damzyt <damianzytner@gmail.com>
2025-01-29 02:02:55 +0100

    Additional better-auth.js config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,12 +8,66 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
         requireEmailVerification: true,
+        autoSignInAfterVerification: true,
+    },
+    emailVerification: {
+        sendOnSignUp: true,
+        sendOnEmailChange: true,
     },
     socialProviders: {
         discord: {
             clientId: process.env.DISCORD_CLIENT_ID as string,
             clientSecret: process.env.DISCORD_CLIENT_SECRET as string
         }
-    }
+    },
+    user: {
+        modelName: 'cardinal_users',
+        fields: {
+            emailVerified: 'email_verified',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        }
+    },
+    account: {
+        modelName: 'cardinal_user_accounts',
+        fields: {
+            userId: 'user_id',
+            accountId: 'account_id',
+            accessToken: 'access_token',
+            refreshToken: 'refresh_token',
+            accessTokenExpiresAt: 'access_token_expires_at',
+            refreshTokenExpiresAt: 'refresh_token_expires_at',
+            idToken: 'id_token',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
+        accountLinking: {
+            enabled: true,
+        }
+    },
+    session: {
+        modelName: 'cardinal_user_sessions',
+        fields: {
+            userId: 'user_id',
+            createdAt: 'created_at',
+            ipAddress: 'ip_address',
+            userAgent: 'user_agent',
+            updatedAt: 'updated_at',
+            expiresAt: 'expires_at',
+        },
+        cookieCache: {
+            enabled: true,
+            maxAge: 5 * 60,
+        }
+    },
+    verification: {
+        modelName: 'cardinal_user_verifications',
+        fields: {
+            expiresAt: 'expires_at',
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        }
+    },
 });
